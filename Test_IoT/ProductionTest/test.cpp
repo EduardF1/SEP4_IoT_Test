ec51f0658cb0079f9ae9b583706e0bf9573b8332
Toma Chan <tomachan.psc@gmail.com>
2020-12-13 00:06:45 +0100

    PIR tests removed as it is hard to test

--- Test_IoT/ProductionTest/test.cpp
+++ Test_IoT/ProductionTest/test.cpp
@@ -11,7 +11,6 @@ extern "C"
 #include "drivers/hcSr501.h"
 #include "temp_hum_sensor.h"
 #include "co2.h"
-#include "pir_sensor.h"
 
 #include <stdbool.h>
 }
@@ -24,31 +23,6 @@ extern "C"
 
 */
 
-
-
-/// <summary>
-/// PIR SENSOR TEST SUITE
-/// </summary>
-
-
-//	Fake function to create the PIR Sensor
-FAKE_VALUE_FUNC(hcsr501_p, hcsr501_create, volatile uint8_t*, uint8_t);
-
-FAKE_VALUE_FUNC(bool, hcsr501_isDetecting, hcsr501_p);
-
-class PIR_Test : public ::testing::Test
-{
-protected:
-	void SetUp() override
-	{
-		FFF_RESET_HISTORY();
-	}
-
-	void TearDown() override
-	{
-
-	}
-};
 
 /// <summary>
 /// Temp_Hum SENSOR TEST SUITE
@@ -110,45 +84,54 @@ protected:
 
 };
 
+TEST_F(Temp_Hum_Test, Test_initialize_Temp_Hum_driver) {
 
-TEST_F(Temp_Hum_Test, Test_Temp_Hum_initialize_driver_is_called) {
 	//	Arrange
+
 	//	Act
 	temp_hum_initalizeDriver();
 
 	//	Assert
 	ASSERT_EQ(1, hih8120_create_fake.call_count);
-	ASSERT_TRUE(0 == hih8120_create_fake.return_val);
+	ASSERT_TRUE(HIH8120_OK == hih8120_create_fake.return_val);
 }
 
-TEST_F(Temp_Hum_Test, Test_Temp_Hum_Create_driver_and_get_temperature_humidity) {
+TEST_F(Temp_Hum_Test, Test_get_temperature_humidity) {
+
 	//	Arrange
-	hih8120_create();
 
 	//	Act
 	int16_t lastTemperature = temp_hum_getLatestTemperature();
 	uint16_t lastHumidity = temp_hum_getLatestHumidity();
 
+	auto wakeUp_rc = temp_hum_wake_up();
+	auto measure_rc = temp_hum_measure();
+
 	//	Assert
-	EXPECT_TRUE(1 == hih8120_create_fake.call_count);
-	ASSERT_TRUE(0 == temp_hum_wake_up());
-	ASSERT_TRUE(0 == temp_hum_measure());
+	ASSERT_TRUE(HIH8120_OK == wakeUp_rc);
+	ASSERT_TRUE(HIH8120_OK == measure_rc);
 	ASSERT_FALSE(lastTemperature == temp_hum_getLatestHumidity());
 	ASSERT_FALSE(lastHumidity == temp_hum_getLatestTemperature());
+	ASSERT_TRUE(456 == temp_hum_getLatestHumidity());
+	ASSERT_TRUE(550 == temp_hum_getLatestTemperature());
 }
 
+TEST_F(CO2_Test, Test_get_CO2_value) {
 
-TEST_F(CO2_Test, Test_CO2_create_and_get_CO2_value) {
 	//	Arrange
 	int co2ppm;
+
 	//	Act
 	co2ppm = co2_getLastCO2ppm();
+
 	//	Assert
 	EXPECT_EQ(0, co2ppm);
 }
 
 TEST_F(CO2_Test, Test_CO2_measure_is_called_10_times) {
+
 	//	Arrange
+
 	//	Act
 	for (int i = 0; i < 10; i++)
 	{
@@ -157,44 +140,20 @@ TEST_F(CO2_Test, Test_CO2_measure_is_called_10_times) {
 
 	//	Assert
 	ASSERT_EQ(10, mh_z19_takeMeassuring_fake.call_count);
+	ASSERT_TRUE(MHZ19_OK == mh_z19_takeMeassuring_fake.return_val);
 }
 
 TEST_F(CO2_Test, Test_CO2_set_data_is_called) {
+
 	//	Arrange
+	uint16_t value = co2_getLastCO2ppm();
 
 	//	Act
-
-	uint16_t value = co2_getLastCO2ppm();
 	co2_getLatestMeasurement();
 
 	//	Assert
 	ASSERT_TRUE(mh_z19_getCo2Ppm_fake.call_count == 1);
-	EXPECT_TRUE(0 == mh_z19_getCo2Ppm_fake.return_val);
+	EXPECT_TRUE(MHZ19_OK == mh_z19_getCo2Ppm_fake.return_val);
 	ASSERT_TRUE(1500 == co2_getLastCO2ppm());
 	ASSERT_FALSE(value == co2_getLastCO2ppm());
-}
-
-
-TEST_F(PIR_Test, Test_PIR_sensor_is_created) {
-	//	Arrange
-	//	Act
-	pir_createSensor();
-	hcsr501_p instance = get_pir_instance();
-
-	//	Assert
-	EXPECT_TRUE(hcsr501_create_fake.call_count == 1);
-	ASSERT_TRUE(instance == get_pir_instance());
-}
-
-TEST_F(PIR_Test, Test_PIR_sensor_is_detecting) {
-	//	Arrange
-	//	Act
-	pir_createSensor();
-	hcsr501_p instance = get_pir_instance();
-
-	//	Assert
-	//EXPECT_FALSE(hcsr501_isDetecting_fake.return_val);
-	ASSERT_FALSE(hcsr501_isDetecting(instance));
-	//ASSERT_FALSE(isDetecting(instance));
-	ASSERT_TRUE(0 == pir_sensor_getPeopleCount());
 }
